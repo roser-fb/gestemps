3f696f7663d4867e0ddbcc776675df9430484e81
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2024-02-08 14:35:14 +0100

    new api url

--- src/app/fitxar/pipes/tmps-restant.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TmpsRestantPipe } from './tmps-restant.pipe';
-
-describe('TmpsRestantPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TmpsRestantPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
