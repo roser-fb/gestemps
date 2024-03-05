0d0c6c396e8d5b5dfb813ff7dc8d41ad5fb4f591
Roser Fuster Bertomeu <rfuster@uoc.edu>
2024-03-05 18:29:25 +0100

    navbar sin logs

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -26,22 +26,13 @@ export class NavbarComponent {
   userRoleIn(allowedRoles: Role[]): boolean {
     const id = this.AuthStoreService.getUserId();
     let res = false;
-    let usera;
-    let aRoles;
-    let uRole: Role | undefined = undefined;
     if (!id) return res;
     this.userService.getUserById(id).pipe(
       map((user) => {
-        usera = user;
-        aRoles = allowedRoles;
-        if(user) uRole = user.role;
+        if(user)
         res = Boolean(user && allowedRoles.includes(user.role));
       })
-    );
-    console.log(res);
-    console.log(usera);
-    console.log(aRoles);
-    console.log(uRole);    
+    ); 
     return res;
   }
 
