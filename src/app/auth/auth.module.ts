ff619f46462e3d15c67127c6173ba05f5cd910f8
Roser Fuster Bertomeu <rfusterb@uoc.edu>
2024-03-05 13:33:00 +0100

    JWTModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,10 +6,21 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { AppComponent } from "../app.component";
 import { LoginComponent } from "./components/login/login.component";
-
+import { JwtModule, JWT_OPTIONS } from "@auth0/angular-jwt";
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      jwtOptionsProvider: {
+        provide: JWT_OPTIONS,
+        useValue: {},
+      },
+    }),
+  ],
   providers: [AuthStoreService, AuthService],
   bootstrap: [AppComponent],
 })
